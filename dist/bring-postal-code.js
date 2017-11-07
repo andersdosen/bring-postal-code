c0cfbc88f8412833375e47af66f9e0682fac1d17
andersdosen <anders.dosen@appex.no>
2017-11-07 18:35:57 +0100

    Added country parameter, updated example with init code

--- dist/bring-postal-code.js
+++ dist/bring-postal-code.js
@@ -1,48 +1,61 @@
 'use strict';
 
-var bringZipCodes = document.querySelectorAll('.bring-zip');
-var bringResultInputs = document.querySelectorAll('.bring-result');
-var bringClientURL = window.location.protocol + '//' + window.location.host + '/';
-var bringCount = void 0;
+bringPostalCode();
 
-if (bringZipCodes.length && bringResultInputs.length) {
-	var _loop = function _loop() {
+function bringPostalCode(bringArgs) {
 
-		var bringZipType = bringZipCodes[bringCount].nodeName;
-		var bringResultType = bringResultInputs[bringCount].nodeName;
-		var bringZipInput = void 0;
-		var bringResultInput = void 0;
+	if (typeof bringArgs === 'undefined') {
+		var bringArgs = {};
+	}
 
-		if (bringZipType === 'INPUT') {
-			bringZipInput = bringZipCodes[bringCount];
-		} else {
-			bringZipInput = bringZipCodes[bringCount].querySelector('input');
-		}
+	if (!bringArgs.country) {
+		bringArgs.country = 'no';
+	}
 
-		if (bringResultType === 'INPUT') {
-			bringResultInput = bringResultInputs[bringCount];
-		} else {
-			bringResultInput = bringResultInputs[bringCount].querySelector('input');
-		}
+	var bringZipCodes = document.querySelectorAll('.bring-zip');
+	var bringResultInputs = document.querySelectorAll('.bring-result');
+	var bringClientURL = window.location.protocol + '//' + window.location.host + '/';
+	var bringCount = void 0;
 
-		bringZipInput.addEventListener('keyup', function () {
+	if (bringZipCodes.length && bringResultInputs.length) {
+		var _loop = function _loop() {
 
-			var bringZipValue = parseInt(this.value);
+			var bringZipType = bringZipCodes[bringCount].nodeName;
+			var bringResultType = bringResultInputs[bringCount].nodeName;
+			var bringZipInput = void 0;
+			var bringResultInput = void 0;
 
-			fetch('https://api.bring.com/shippingguide/api/postalCode.json?clientUrl=' + bringClientURL + '&pnr=' + bringZipValue).then(bringCheckStatus).then(bringGetJSON).then(function (data) {
-				if (data.valid === true) {
-					bringResultInput.value = data.result;
-				} else {
-					bringResultInput.value = '';
-				}
-			}).catch(function (err) {
-				console.log('ERROR', err);
+			if (bringZipType === 'INPUT') {
+				bringZipInput = bringZipCodes[bringCount];
+			} else {
+				bringZipInput = bringZipCodes[bringCount].querySelector('input');
+			}
+
+			if (bringResultType === 'INPUT') {
+				bringResultInput = bringResultInputs[bringCount];
+			} else {
+				bringResultInput = bringResultInputs[bringCount].querySelector('input');
+			}
+
+			bringZipInput.addEventListener('keyup', function () {
+
+				var bringZipValue = parseInt(this.value);
+
+				fetch('https://api.bring.com/shippingguide/api/postalCode.json?clientUrl=' + bringClientURL + '&pnr=' + bringZipValue + '&country=' + bringArgs.country).then(bringCheckStatus).then(bringGetJSON).then(function (data) {
+					if (data.valid === true) {
+						bringResultInput.value = data.result;
+					} else {
+						bringResultInput.value = '';
+					}
+				}).catch(function (err) {
+					console.log('ERROR', err);
+				});
 			});
-		});
-	};
+		};
 
-	for (bringCount = 0; bringZipCodes.length > bringCount; bringCount++) {
-		_loop();
+		for (bringCount = 0; bringZipCodes.length > bringCount; bringCount++) {
+			_loop();
+		}
 	}
 }
 
--- src/bring-postal-code.js
+++ src/bring-postal-code.js
@@ -1,54 +1,66 @@
 'use strict';
 
-const bringZipCodes = document.querySelectorAll('.bring-zip');
-const bringResultInputs = document.querySelectorAll('.bring-result');
-const bringClientURL = window.location.protocol + '//' + window.location.host + '/';
-let bringCount;
+function bringPostalCode(bringArgs) {
 
-if (bringZipCodes.length && bringResultInputs.length) {
-	for (bringCount = 0; bringZipCodes.length > bringCount; bringCount++) {
-
-		let bringZipType = bringZipCodes[bringCount].nodeName;
-		let bringResultType = bringResultInputs[bringCount].nodeName;
-		let bringZipInput;
-		let bringResultInput;
-
-		if (bringZipType === 'INPUT') {
-			bringZipInput = bringZipCodes[bringCount];
-		}
-		else {
-			bringZipInput = bringZipCodes[bringCount].querySelector('input');
-		}
-
-		if (bringResultType === 'INPUT') {
-			bringResultInput = bringResultInputs[bringCount];
-		}
-		else {
-			bringResultInput = bringResultInputs[bringCount].querySelector('input');
-		}
-
-		bringZipInput.addEventListener('keyup', function() {
-
-			let bringZipValue = parseInt(this.value);
-
-			fetch('https://api.bring.com/shippingguide/api/postalCode.json?clientUrl=' + bringClientURL + '&pnr=' + bringZipValue)
-				.then(bringCheckStatus)
-				.then(bringGetJSON)
-				.then(function(data) {
-					if (data.valid === true) {
-						bringResultInput.value = data.result;
-					}
-					else {
-						bringResultInput.value = '';
-					}
-				})
-				.catch(function(err) {
-					console.log('ERROR', err);
-				});
-
-		});
-
+	if (typeof bringArgs === 'undefined') {
+		var bringArgs = {};
 	}
+
+	if (!bringArgs.country) {
+		bringArgs.country = 'no';
+	}
+
+	const bringZipCodes = document.querySelectorAll('.bring-zip');
+	const bringResultInputs = document.querySelectorAll('.bring-result');
+	const bringClientURL = window.location.protocol + '//' + window.location.host + '/';
+	let bringCount;
+
+	if (bringZipCodes.length && bringResultInputs.length) {
+		for (bringCount = 0; bringZipCodes.length > bringCount; bringCount++) {
+
+			let bringZipType = bringZipCodes[bringCount].nodeName;
+			let bringResultType = bringResultInputs[bringCount].nodeName;
+			let bringZipInput;
+			let bringResultInput;
+
+			if (bringZipType === 'INPUT') {
+				bringZipInput = bringZipCodes[bringCount];
+			}
+			else {
+				bringZipInput = bringZipCodes[bringCount].querySelector('input');
+			}
+
+			if (bringResultType === 'INPUT') {
+				bringResultInput = bringResultInputs[bringCount];
+			}
+			else {
+				bringResultInput = bringResultInputs[bringCount].querySelector('input');
+			}
+
+			bringZipInput.addEventListener('keyup', function() {
+
+				let bringZipValue = parseInt(this.value);
+
+				fetch('https://api.bring.com/shippingguide/api/postalCode.json?clientUrl=' + bringClientURL + '&pnr=' + bringZipValue + '&country=' + bringArgs.country)
+					.then(bringCheckStatus)
+					.then(bringGetJSON)
+					.then(function(data) {
+						if (data.valid === true) {
+							bringResultInput.value = data.result;
+						}
+						else {
+							bringResultInput.value = '';
+						}
+					})
+					.catch(function(err) {
+						console.log('ERROR', err);
+					});
+
+			});
+
+		}
+	}
+
 }
 
 function bringCheckStatus(response) {
