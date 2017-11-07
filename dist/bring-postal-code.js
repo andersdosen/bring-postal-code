3b3552693b7924ed883e8bbe66e11b10e7ec1acd
andersdosen <anders.dosen@appex.no>
2017-11-07 14:35:05 +0100

    Project files

---
+++ dist/bring-postal-code.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Postal Code Lookup – https://developer.bring.com/api/postal-code/#lookup-postal-code
+const bringZipCodes = document.querySelectorAll('.bring-zip');
+const bringResultInputs = document.querySelectorAll('.bring-result');
+const bringClientURL = 'https://google.com/';
+let bringCount;
+
+if (bringZipCodes.length) {
+	for (bringCount = 0; bringZipCodes.length > bringCount; bringCount++) {
+
+		let bringZipInput = bringZipCodes[bringCount].querySelector('input');
+		let bringResultItem = bringResultInputs[bringCount];
+		let bringResultInput = bringResultItem.querySelector('input');
+
+		bringZipInput.addEventListener('keyup', function() {
+
+			let bringZipValue = parseInt(this.value);
+
+			fetch('https://api.bring.com/shippingguide/api/postalCode.json?clientUrl=' + bringClientURL + '&pnr=' + bringZipValue)
+				.then(bringCheckStatus)
+				.then(bringGetJSON)
+				.then(function(data) {
+					if (data.valid === true) {
+						bringResultInput.value = data.result;
+					}
+					else {
+						bringResultInput.value = '';
+					}
+				})
+				.catch(function(err) {
+					console.log('ERROR', err);
+				});
+
+		});
+
+	}
+}
+
+function bringCheckStatus(response) {
+	if (response.status === 200) {
+		return Promise.resolve(response);
+	}
+	else {
+		return Promise.reject(
+			new Error(response.statusText)
+		);
+	}
+}
+
+function bringGetJSON(response) {
+	return response.json();
+}
